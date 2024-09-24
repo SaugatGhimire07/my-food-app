5c378e2718a95d3aa566bdc599a6810d7eb9e55b
Saugat <ghimiresaugat987@gmail.com>
2024-09-23 20:08:28 -0400

    added title

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import Header from './components/Layout/Header';
-import Meals from './components/Meals/Meals';
-import CartProvider from './store/CartProvider';
-
+import { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
   return (
     <CartProvider>
-      <Header />
+      <h1>made by saugat</h1>
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
     </CartProvider>
   );
 }
-
 export default App;
